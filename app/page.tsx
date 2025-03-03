f54fd7b141161d90f192a20a26d10dba5a6a76f3
Shahriyar <shahryar.tarnasi@gmail.com>
2025-03-04 00:51:39 +0400

    design fixed

--- app/page.tsx
+++ app/page.tsx
@@ -19,19 +19,19 @@ export default function Home() {
       {/* Links Section */}
       <div className="bg-amber-900 flex flex-col justify-center items-center h-full text-white">
         <ul className="space-y-10 w-3/4 text-center">
-          <li className="p-2 border-b-2">
+          <li className="p-2 border-b-2 hover:bg-amber-800">
             <Link href="/blockchain" className="text-3xl">My Resume</Link>
           </li>
-          <li className="p-2 border-b-2">
+          <li className="p-2 border-b-2 hover:bg-amber-800">
             <Link href="/projects" className="text-3xl">Projects</Link>
           </li>
-          <li className="p-2 border-b-2">
+          <li className="p-2 border-b-2 hover:bg-amber-800">
             <Link href="/crypto" className="text-3xl">Crypto and Market</Link>
           </li>
-          <li className="p-2 border-b-2">
+          <li className="p-2 border-b-2 hover:bg-amber-800">
             <Link href="/blockchain" className="text-3xl">Blockchain and Smart Contract</Link>
           </li>
-          <li className="p-2 border-b-2">
+          <li className="p-2 border-b-2 hover:bg-amber-800">
             <Link href="/contact" className="text-3xl">Contact me</Link>
           </li>
         </ul>
