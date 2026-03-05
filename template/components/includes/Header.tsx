68bd03d54c4f98bd3635c86fc2213bc6d78fa959
shahriyar <shahryar.tarnasi@gmail.com>
2026-03-06 00:32:31 +0400

    the header and main page started

---
+++ template/components/includes/Header.tsx
@@ -0,0 +1,21 @@
+'use client';
+import Image from 'next/image'
+
+
+export function MainHeader() {
+  return (
+    <header className="flex items-center justify-between w-full h-20 px-10 bg-blue-300 text-white rounded-full border shadow-lg">
+      <div className="flex items-center space-x-4">
+        <Image src="/next.svg" alt="Logo" width={120} height={120} />
+      </div>
+      <nav>
+        <ul className="flex space-x-4">
+          <li><a href="#" className="hover:underline text-lg font-bold">Home</a></li>
+          <li><a href="#" className="hover:underline text-lg font-bold">Projects</a></li>
+          <li><a href="#" className="hover:underline text-lg font-bold">Blog</a></li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
+
